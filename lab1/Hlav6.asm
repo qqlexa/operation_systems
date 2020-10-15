2c47eac716033d58972814f33a15404fb9dd1d87
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-10-15 23:14:05 +0300

    Add files via upload

---
+++ lab1/Hlav6.asm
@@ -0,0 +1,50 @@
+title Hlav6.asm
+
+.MODEL tiny
+.DATA
+
+
+; 1833 1821
+; 948 936
+; 91 79
+; 148 136
+
+; byte
+i db 0, 22
+i1 db -255
+ii db -128, 127
+
+; my values
+i2 db 136
+i3 db 148
+i4 db 79
+i5 db 91
+
+;//
+
+; word
+
+r0 dw -255
+r dw 0, 65535
+r1 dw -1, -32768
+r2 dw 1, 32767
+r3 dw -65535
+r4 dd -65535
+
+
+
+; my values
+ri2 dw 136
+ri3 dw 148
+ri4 dw 79
+ri5 dw 91
+
+r5 dw 1833
+r6 dw 1821
+r7 dw 936
+r8 dw 948
+
+;//
+
+
+end

db76bd88df0f0faf755b9e01fd03ecf7d165a371
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-10-16 12:00:18 +0300

    Add files via upload

---
+++ lab2/Second.asm
@@ -0,0 +1,21 @@
+text	segment			
+assume 	CS:text, DS:data	
+					
+begin:	mov	AX, data		
+	mov	DS, AX		
+	mov	AH, 09h		
+	mov	DX, offset mesg
+	int 	21h			
+	mov	AH, 4Ch		
+	mov Al, 0			
+	int		21h			
+text	ends				
+
+data	segment			
+mesg	db "NACHINAEM ! $"
+data	ends				
+
+stk	segment stack		
+	db 256 dup (0)		
+stk	ends				
+	end begin			
